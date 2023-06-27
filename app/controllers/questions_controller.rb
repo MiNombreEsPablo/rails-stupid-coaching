76e6896c87294349c9efa8449cbf9a798a0c5bc9
Pablo Bugueno <pablo.buguenoe@gmail.com>
2023-06-27 20:23:51 +0900

    logic in controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,8 +6,27 @@ class QuestionsController < ApplicationController
   def ask; end
 
   def answer
-    @question = ''
+    @question = params[:question]
+    @reply = coach_answer_enhanced(@question)
+  end
 
-    @question = params[:question] if params[:question]
+  private
+
+  def coach_answer(your_message)
+    if your_message.capitalize == 'I am going to work right now!'
+      'See you there.'
+    elsif your_message[-1] == '?'
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def coach_answer_enhanced(your_message)
+    if your_message == your_message.upcase
+      "I can feel your motivation! #{coach_answer(your_message)}"
+    else
+      coach_answer(your_message)
+    end
   end
 end
